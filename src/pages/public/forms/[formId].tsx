772dd07402f1faf7a9ddc30f7c929f7274a035dc
Daniel Markow <d_markow@icloud.com>
2023-03-20 08:08:24 +0100

    dynamically generate zod schema to validate public form

--- src/pages/public/forms/[formId].tsx
+++ src/pages/public/forms/[formId].tsx
@@ -1,17 +1,43 @@
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { api } from "~/utils/api";
 import RenderField from "~/components/RenderField";
+
+const mapFieldTypeToZod = (fieldType: string) => {
+  // TODO possibly expand to more validation options configured by the form creator
+  switch (fieldType) {
+    case "text":
+      return z.string();
+    case "number":
+      return z.number();
+    case "select":
+      return z.string();
+    case "radio":
+      return z.string();
+  }
+};
+
+let formSchema = z.object({});
 
 export default function PublicForm() {
   const router = useRouter();
   const { formId } = router.query;
 
+  let schemaObj = {};
+
   const publicFormQuery = api.form.getPublicForm.useQuery(
     { formId: formId as string },
     {
       enabled: formId !== undefined,
+      onSuccess: (data) => {
+        data?.fields.forEach((f) =>
+          Object.assign(schemaObj, { [f.name]: mapFieldTypeToZod(f.type) })
+        );
+        formSchema = z.object(schemaObj);
+      },
     }
   );
 
@@ -21,17 +47,17 @@ export default function PublicForm() {
     formState: { errors },
     watch,
     setValue,
-  } = useForm();
+  } = useForm({ resolver: zodResolver(formSchema) });
 
   return (
     <>
       <div>
-        {publicFormQuery.isSuccess && JSON.stringify(publicFormQuery.data)}
+        {/* {publicFormQuery.isSuccess && JSON.stringify(publicFormQuery.data)} */}
         <br />
-        {publicFormQuery.isSuccess &&
+        {/* {publicFormQuery.isSuccess &&
           JSON.stringify(publicFormQuery.data?.fields.map((f) => f.type))}
         {publicFormQuery.isSuccess &&
-          JSON.stringify(publicFormQuery.data?.fields.map((f) => f.name))}
+          JSON.stringify(publicFormQuery.data?.fields.map((f) => f.name))} */}
 
         <h1 className="text-xl">
           {publicFormQuery.isSuccess && publicFormQuery.data!.name}
