5f7e7c2fe034be2a11beaaf1c9ee37638f24a521
Daniel Markow <d_markow@icloud.com>
2023-03-22 16:41:07 +0100

    receiving public form data

--- src/server/api/routers/formDataRouter.ts
+++ src/server/api/routers/formDataRouter.ts
@@ -1,4 +1,22 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
-export const formDataRouter = createTRPCRouter({});
+export const formDataRouter = createTRPCRouter({
+  createEntry: publicProcedure
+    .input(
+      z.object({
+        formId: z.string().cuid(),
+        updatedAt: z.date(),
+        submission: z.string(),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.publicFormData.create({
+        data: {
+          form: { connect: { id: input.formId } },
+          lastUpdatedForm: input.updatedAt,
+          submission: input.submission,
+        },
+      });
+    }),
+});
